36b1b29df14c91d81c8237608314ec07c87bed4b
kenroybeats <kenroy.beats@beatsbydre.com>
2014-05-20 12:50:12 -0700

    partials

--- app.js
+++ app.js
@@ -3,26 +3,27 @@ var express = require('express');
 
 var app = express();
 
+app.locals.pagetitle = "Awesome Website";
+
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-    res.render('default', {
-        title: 'Home',
-        classname: 'home',
-        users: ['Ray', 'Morten', 'James']
-    });
+
+
+app.get('/', routes.index);
+
+app.get('/about', routes.about);
+
+app.get('*', function(req, res) {
+    res.send('Bad Route');
 });
 
-app.get('/me', function(req, res) {
-    res.send('<H1>Hello me! I AM COOL</H1>');
-});
 
-//example - http://localhost:3000/you/kenroy
-app.get('/you/:name?', function(req, res) {
-    var name = req.params.name;
-    res.send('<H1>Hello</H1>' + name);
-});
-
+////example - http://localhost:3000/you/kenroy
+//app.get('/you/:name?', function(req, res) {
+//    var name = req.params.name;
+//    res.send('<H1>Hello</H1>' + name);
+//});
+//
 
 //start server
 
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+//index becomes routes.index in app.js
+exports.index = function(req, res) {
+  res.render('default', {
+    title: 'Home',
+    classname: 'home',
+    users: ['Ray', 'Morten', 'James']
+  });
+}
+
+exports.about = function(req, res) {
+  res.render('default', {
+    title: 'About Us',
+    classname: 'about'
+  });
+}
