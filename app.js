3bc1f893fa7c3147ad73453fa36e6b16b89078d6
kenroybeats <kenroy.beats@beatsbydre.com>
2014-05-20 11:34:39 -0700

    made server

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-var http = require('http');
 
-var myServer = http.createServer( function (request, response) {
-    response.writeHead(200, {"Content-Type" : "text/html"});
-    response.write("<b>HELLLOOOOOO WORLD!</b>");
-    response.end();
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('<H1>Hello mannnnnnn</H1> Express');
 });
 
-myServer.listen(3000);
 
-console.log("go to http://localhost:3000 on your browser");
+
+app.get('/me', function(req, res) {
+    res.send('<H1>Hello me! I AM COOL</H1>');
+});
+
+app.get('/you/:name?', function(req, res) {
+    var name = req.params.name;
+    res.send('<H1>Hello</H1>' + name);
+});
+
+
+var server = app.listen(3000, function() {
+    console.log("I am listening on 3000");
+});
