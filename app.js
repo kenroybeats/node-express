68fa767f813399f342a89fa6642b6e1b6b4bdfd6
kenroybeats <kenroy.beats@beatsbydre.com>
2014-05-20 12:40:03 -0700

    express templates

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 
 var express = require('express');
+
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-    res.send('<H1>Hello mannnnnnn</H1> Express');
+    res.render('default', {
+        title: 'Home',
+        users: ['Ray', 'Morten', 'James']
+    });
 });
-
-
 
 app.get('/me', function(req, res) {
     res.send('<H1>Hello me! I AM COOL</H1>');
 });
 
+//example - http://localhost:3000/you/kenroy
 app.get('/you/:name?', function(req, res) {
     var name = req.params.name;
     res.send('<H1>Hello</H1>' + name);
 });
 
+
+//start server
 
 var server = app.listen(3000, function() {
     console.log("I am listening on 3000");
